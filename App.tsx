e1ff211e4c51694f4bc15e83a31e9b4900949d24
sooraj888 <soorajsagar888@gmail.com>
2025-07-02 02:00:57 +0530

    Added some basic layout design

--- App.tsx
+++ App.tsx
@@ -6,14 +6,58 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [message, setMessage] = useState<any>({
+    message: 'Calling test API...',
+  });
+  const callTestApi = async () => {
+    console.log('Calling test API...');
+    try {
+      const response = await fetch('http://192.168.1.111:3000/test/3');
+      const data = await response.json();
+      setMessage(data);
+      console.log('API Response:', data);
+    } catch (error) {
+      console.log('Error calling API:', error);
+    }
+  };
+
+  useEffect(() => {
+    callTestApi();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text
+        style={{
+          color: isDarkMode ? 'white' : 'black',
+          textAlign: 'center',
+          marginTop: 20,
+        }}
+      >
+        {JSON.stringify(message, null, 2)}
+      </Text>
+      <Button
+        title="Call API"
+        onPress={() => {
+          setMessage({ message: 'Calling test API...' });
+
+          callTestApi();
+        }}
+      ></Button>
       <NewAppScreen templateFileName="App.tsx" />
     </View>
   );
